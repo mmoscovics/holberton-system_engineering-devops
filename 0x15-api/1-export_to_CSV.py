3596be9addf096c7fb7e4502e530665d6c6b2164
Mitchell <mmoscovics@yahoo.com>
2020-05-04 22:02:55 +0000

    [Bugfix] correct data

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,11 +9,12 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/" + user_id).json()
     name = user["name"]
+    user_name = user["username"]
     tasks = requests.get(url + "todos?userId=" + user_id).json()
     with open(user_id + ".csv", "w") as file:
         for task in tasks:
             status = task["completed"]
             title = task["title"]
-            format = '"{}","{}","{}","{}"\n'.format(user_id,
-                                                    name, status, title)
+            format = '"{}","{}","{}","{}"\n'.format(user_id, user_name,
+                                                    status, title)
             file.write(format)
