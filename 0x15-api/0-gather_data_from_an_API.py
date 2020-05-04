a3ec85ba7cabb2874aaaa807b13d52f9460712a8
Mitchell <mmoscovics@yahoo.com>
2020-05-04 21:44:41 +0000

    [Update] pep8 compliance

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
     done = 0
     total = 0
     for task in tasks:
-        if task["completed"] == True:
+        if task["completed"] is True:
             done += 1
         total += 1
-    titles = [task for task in tasks if task["completed"] == True]
+    titles = [task for task in tasks if task["completed"] is True]
     print("Employee {} is done with tasks({}/{}):".format(name, done, total))
     for title in titles:
         print("\t {}".format(title["title"]))
