296abee5e12ddbd98055f10a0423c91acd3952ef
Mitchell <mmoscovics@yahoo.com>
2020-05-07 22:09:25 +0000

    [Bugfix] after parameter

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -12,13 +12,13 @@ def recurse(subreddit, hot_list=[], after=""):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     user_agent = {"User-Agent": "Hello There 1.0"}
     response = requests.get(url, headers=user_agent, allow_redirects=False,
-                            )
+                            params={"after": after})
     if response.status_code != 200:
         return None
     else:
         children = response.json()["data"]["children"]
         hot_list.append([title["data"]["title"] for title in children])
         after = response.json()["data"]["after"]
-        if after is None:
-            return hot_list
-        return recurse(subreddit, hot_list, after)
+    if after is None:
+        return hot_list
+    return recurse(subreddit, hot_list, after)
