a358a1dc6df26e5f7465c159ba33e2cb164176bf
Mitchell <mmoscovics@yahoo.com>
2020-05-07 19:46:59 +0000

    [Create] task 0 reddit api get subreddit subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Queries the Reddit API
+returns the number of subscribers for a given subreddit.
+if invalid subreddit is given return 0.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns number of subscribers or 0"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    user_agent = {"User-Agent": "Hello There 1.0"}
+    response = requests.get(url, headers=user_agent, allow_redirects=False).json()
+    all_data = response["data"]
+    subs = all_data["subscribers"]
+    return subs
