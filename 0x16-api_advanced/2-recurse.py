89d354768596fc85f0a73eeb3327e52dee78be20
Mitchell <mmoscovics@yahoo.com>
2020-05-07 21:55:42 +0000

    [Create] task 2 reddit api recursively get hot articles

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Queries the Reddit API
+returns list of the titles of the hot articles for given subreddit.
+if not valid subreddit return None.
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """Returns list of hot article titles of given subreddit."""
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    user_agent = {"User-Agent": "Hello There 1.0"}
+    response = requests.get(url, headers=user_agent, allow_redirects=False,
+                            )
+    if response.status_code != 200:
+        return None
+    else:
+        children = response.json()["data"]["children"]
+        hot_list.append([title["data"]["title"] for title in children])
+        after = response.json()["data"]["after"]
+        if after is None:
+            return hot_list
+        return recurse(subreddit, hot_list, after)
